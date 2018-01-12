d8822cb1d48b4d061918fcb90b4ce38197d5ba8b
zhanghaisheng <zhanghaisheng@chinamobiad.com>
2018-01-12 09:42:05 +0800

    update

--- src/main/scala/scala_pb/PBTest.scala
+++ src/main/scala/scala_pb/PBTest.scala
@@ -1,9 +1,32 @@
 package scala_pb
 
+import java.util.Base64
+
+import com.chinamobiad.adx.idl.{Bool, DspApiType, IntervalHour, Placement}
+
+/**
+  * pb 编译依赖python
+  */
 object PBTest {
 
   def main(args: Array[String]): Unit = {
-    
+
+    val p = Placement(id = "id1",
+      pubAppId = 1,
+      dspApiType = DspApiType.C2S,
+      ecpm = 1.2,
+      isAgApp = Bool.True,
+      dspCode = 1,
+      activeIntervalsForDsp = List(IntervalHour(startAt = 1,endAt = 2)),
+      extData = Map("k1" -> "v1"))
+
+    val b = p.toByteArray
+    val p2 = Placement.parseFrom(b)
+    println(p2)
+
+    val s = Base64.getEncoder.encodeToString(b)
+    val p3 = Placement.parseFrom(Base64.getDecoder.decode(s))
+    println(p3)
   }
 
 }
--- src/test/scala/slick/UserDaoSpec.scala
+++ src/test/scala/slick/UserDaoSpec.scala
@@ -5,7 +5,7 @@ import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 class UserDaoSpec extends WordSpecLike
                   with BeforeAndAfterAll
                   with Matchers {
-  
+
   "first test" in {
     val r = UserDao.findById(1)
     r shouldEqual(1, "张三")
