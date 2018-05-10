ed31958e6acd365b814ec80192fb888571a28279
hamlt <332627460@qq.com>
2018-05-10 18:41:42 +0800

    update

--- src/main/scala/AkAkHttp/TemplateMsgHttpsTest.scala
+++ src/main/scala/AkAkHttp/TemplateMsgHttpsTest.scala
@@ -1,68 +1,70 @@
 package AkAkHttp
 
-
-import java.io.{File, FileOutputStream}
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
-import akka.http.scaladsl.model.headers.RawHeader
 import akka.stream.ActorMaterializer
 import akka.util.ByteString
-import scala.concurrent.Future
-import io.circe.parser._
 import io.circe.generic.auto._
+import io.circe.parser._
+import io.circe.syntax._
 
-object WxQrCodeHttpsTest {
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
+import scala.util.{Failure, Success}
+
+object TemplateMsgHttpsTest {
   val appid = "wxa392f9af221825c3"
   val secret = "411a8fc21cae8b8834254adc19be896a"
-  //val appid = "wx735c5d5f6dd497a9"
-  //val secret = "700f86314f8703f616983fee7adc2d2a"
+
   implicit val system = ActorSystem()
   implicit val mat = ActorMaterializer()
   implicit val executionContext = system.dispatcher
 
   final case class Token(access_token: String, expires_in: Long)
+  final case class Pair(value: String, color: String = "#173177")
+  final case class MsgTemplate(touser: String, template_id: String, page: String, form_id: String, data: Map[String, Pair])
+
 
   def main(args: Array[String]): Unit = {
+    val msgtmp = MsgTemplate(
+      touser = "o9JK35Yrx1kN-dWZHoMNeX73Dmro"
+      , template_id = "EBmgTu0hLAOjHGxBEqiioabAUSLV3ztYEqEkHAHf9lY"
+      , page = "pages/index"
+      , form_id = "c53de3255f52d4ab9396a6ceabae6150" // 支付可以使用3次，普通form只能用1次，有效期7天
+      , data = Map("keyword1" -> Pair("赛季1"), "keyword2" -> Pair("2018-9-9"), "keyword3" -> Pair("深圳"))
+    )
     val f1 = getAccessToken()
-    f1.map {
+    val f2 = f1.flatMap {
       case Some(token) =>
-       //val opendId = "ovH6U0-iqoTNSpkLdV4cq6qEXUQY"//"ovH6U0-ekSODK3lqNyoru2W2KfpI"
-       //val url1 = s"https://api.weixin.qq.com/cgi-bin/user/info?access_token=${token.access_token}&openid=$opendId&lang=zh_CN"
-       //println(url1)
-        val f2 = getImgBytes(token.access_token)
-        f2.map {
-          case Some(x) =>
-            writeToFile(x, path = "/home/hamlt/qrcode.png")
-          case _ =>
-        }
+        val str = msgtmp.asJson.toString()
+        println(str)
+        val result = send(token.access_token, str)
+        result
       case _ =>
+        Future.successful(Some("no token"))
     }
+    f2.onComplete {
+      case Success(r) =>
+        println(r)
+      case Failure(e) =>
+        println(e)
+    }
+    Await.result(f2, Duration.Inf)
   }
 
-  private[this] def writeToFile(bytes: Array[Byte], path: String) = {
-    val file = new File(path)
-    if(!file.exists()){
-      file.createNewFile()
-    }
-    val a: FileOutputStream  = new FileOutputStream(file)
-    a.write(bytes)
-    a.flush()
-    a.close()
-    println("write to file success")
-  }
-
-  private[this] def getImgBytes(token: String): Future[Option[Array[Byte]]] = {
-    val url = s"https://api.weixin.qq.com/cgi-bin/wxaapp/createwxaqrcode?access_token=$token"
-    val entity = HttpEntity(string = "{\"path\": \"pages/index?query=1\", \"width\": 430}", contentType = ContentTypes.`application/json`)
+  private[this] def send(tokenStr: String, templateStr: String) = {
+    val url = s"https://api.weixin.qq.com/cgi-bin/message/wxopen/template/send?access_token=$tokenStr"
+    val entity = HttpEntity(string = templateStr, contentType = ContentTypes.`application/json`)
     val req = HttpRequest(uri = url, method = HttpMethods.POST, entity = entity)
     val responseFuture: Future[HttpResponse] = Http().singleRequest(req)
     responseFuture.flatMap {
       case res: HttpResponse if res.status == StatusCodes.OK =>
-        res.entity.dataBytes.runFold(ByteString(""))(_ ++ _).map(_.toArray).map { bytes =>
-          Some(bytes)
+        val r: Future[Option[String]] = res.entity.dataBytes.runFold(ByteString(""))(_ ++ _).map(_.toArray).map { bytes =>
+          Some(new String(bytes))
         }
-      case _  => Future.successful(None)
+        r
+      case _ => Future.successful(None)
     }
   }
 
@@ -88,4 +90,3 @@ object WxQrCodeHttpsTest {
   }
 
 }
-
--- src/main/scala/comomns/Test1.scala
+++ src/main/scala/comomns/Test1.scala
@@ -1,5 +1,8 @@
 package comomns
 
+import java.text.SimpleDateFormat
+import java.util.Base64
+
 object Test1 {
 
   sealed trait ReqResult
@@ -27,6 +30,27 @@ object Test1 {
       case x => println(x)
     }
 
+
+
+    val current = System.currentTimeMillis
+    val tokenExpireTime = current + (7200 - 3) * 1000
+    val refreshTokenExpireTime = current + ((28L * 24 * 60 * 60) - 3) * 1000
+
+    val sf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+
+
+    println(((25L * 24 * 60 * 60) - 3) * 1000)
+    println(tokenExpireTime + "--" + sf.format(1524913148181L))
+    println(refreshTokenExpireTime + "--" + sf.format(1527325148181L))
+
+    //val s1 = "http%3A%2F%2Ftest.kxdati.com%2F%3FfromType%3D5%26friendsToken%3DeyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1MjU2MTY1NTcsImlhdCI6MTUyNTUzMDE1NywiaWQiOiIxYWQyNTIyMi1hZTRjLTRlZWEtODM4OC01ODljMjBmZjkwNGEiLCJ1aWQiOjExMzd9.NgN9rs8t3nBUrTd2F3H-b_CGjMMdtu1xsZSov08hzSA%26from%3Dsinglemessage%3FfriendsToken%3DeyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE1MjU2MTY1NTcsImlhdCI6MTUyNTUzMDE1NywiaWQiOiIxYWQyNTIyMi1hZTRjLTRlZWEtODM4OC01ODljMjBmZjkwNGEiLCJ1aWQiOjExMzd9.NgN9rs8t3nBUrTd2F3H-b_CGjMMdtu1xsZSov08hzSA%26fromType%3D5,"
+    val s1 = "http%3A%2F%2Ftest.kxdati.com%2F%3FfriendsToken%3Dundefined%26fromType%3Dundefined"
+    val s2 =  java.net.URLDecoder.decode(s1, "utf-8")
+
+    print(s2)
+
+   /* val a: Array[Byte] = Base64.getDecoder.decode("aHR0cDovL3Rlc3Qua3hkYXRpLmNvbS9hcGkvdjAvcmVkaXJlY3Q/ZnJpZW5kVG9rZW49c2RkZGRkZGRkZGRkZGRkZGRkZGRkZGQmZnJpZW5kVHlwZT0x")
+    println(new String(a))*/
   }
 
 }
